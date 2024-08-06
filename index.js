6424ea4f1f73a5db2725c5c38efb777b71c52261
IvanEscobar404 <ivanescobargg@gmail.com>
2024-08-05 21:54:53 -0300

    Terminado del index.js pero falta arreglar algunas funcionalidades como la del menu hamburgesa en la version mobile

--- index.js
+++ index.js
@@ -9,24 +9,54 @@ menuIcon.onClick = () =>{
 
 
 /******************* scroll section active link ************* */
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop = 150;
+        let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
             navLinks.forEach.apply(links => {
-            navLinks.classList.remove('active');
-            document.querySelector('header nav a[href*=' + id + ']')-classList.add('active');
+            links.classList.remove('active');
+            document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
         };
     });
-}
 
 /********************** sticky navbar *********************** */
+let header = document.querySelector('header');
+header.classList.toggle('sticky', window.scrollY > 100);
+
+
+/******************** remove toggle icon and navbar ************************ */
+menuIcon.classList.remove('fa-xmark');
+navbar.classList.remove('active');
+
+};
+
+
+/******************* scroll reveal *********************** */
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay:200
+})
+
+ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'button'});
+ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
+
+
+/************************ typed js ***************************  */
+const typed = new Typed('.multiple-text', {
+    strings: ['software and web developer', 'Web designer', 'Editor de videos'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
