2fd934040e72233b4c931888fe9c598430d14761
IvanEscobar <ivanescobargg@gmail.com>
2024-08-06 21:03:15 -0300

    Agregado de envios de email con EmailJS pero queda ver lo de los styles para el contact-form

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
-menuIcon.onClick = () =>{ 
+menuIcon.onclick = () =>{ 
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active')
 } 
@@ -20,7 +20,7 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height){
-            navLinks.forEach.apply(links => {
+            navLinks.forEach(links => {
             links.classList.remove('active');
             document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             })
@@ -46,17 +46,41 @@ ScrollReveal({
     delay:200
 })
 
-ScrollReveal().reveal('.home-content, heading', {origin: 'top'});
-ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'button'});
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', {origin: 'bottom'});
 ScrollReveal().reveal('.home-contact h1, .about-img', {origin: 'left'});
 ScrollReveal().reveal('.home-contact p, .about-content', {origin: 'right'});
 
 
 /************************ typed js ***************************  */
 const typed = new Typed('.multiple-text', {
-    strings: ['software and web developer', 'Web designer', 'Editor de videos'],
+    strings: ['Software and Web Developer', 'Video Editor'],
     typeSpeed: 70,
     backSpeed: 70,
     backDelay: 1000,
     loop: true,
+});
+
+
+// Initialize EmailJS
+emailjs.init('vsQTVEyE2tQnRCIIl'); // User ID de EmailJS
+
+document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const btn = document.getElementById('button');
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service'; // Service ID de EmailJS
+    const templateID = 'template_rc0izvn'; // Template ID de EmailJS
+
+    emailjs.sendForm(serviceID, templateID, this)
+        .then(() => {
+            btn.value = 'Send Email';
+            alert('Sent!');
+        }, (err) => {
+            btn.value = 'Send Email';
+            alert('Failed to send message. Please try again.');
+            console.error('Error:', err);
+        });
 });
